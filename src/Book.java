e62c66e1b3b4f917a1e5f4f3ea2585a7bbc2fca4
Alina <niimura.tsucumi@gmail.com>
2020-09-16 20:05:30 +0300

    Constructor added 16.09.2020, main project did 11.09.2020

---
+++ src/Book.java
@@ -0,0 +1,44 @@
+public class Book {
+
+    String name;
+    String author;
+    int pages;
+
+    //Я решила добавить конструктор
+    public Book(String name, String author, int pages) {
+        this.name = name;
+        this.author = author;
+        this.pages = pages;
+    }
+
+    public String toString() {
+        String s = '\n'+"Book name - " + name + '\n' + "Author - " + author + '\n' + "Pages - " + pages;
+        return s;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public int getPages() {
+        return pages;
+    }
+
+    public void setPages(int pages) {
+        this.pages = pages;
+    }
+
+
+}
---
+++ src/BookLoader.java
@@ -0,0 +1,15 @@
+public class BookLoader {
+
+    public static void main(String[] arg)
+    {
+
+        Book a= new Book("Портрет Дориана Грея","Оскар Уайльд", 320);
+       /* a.setName("Портрет Дориана Грея");
+        a.setAuthor("Оскар Уайльд");
+        a.setPages(320);
+        решила заменить на конструктор 16.09.2020
+       */
+        System.out.println(a);
+
+    }
+}
